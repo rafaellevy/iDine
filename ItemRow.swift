faf0b75ceb599f0198c576ef69eb3238748601dc
Rafael Levy <srlevy2@gmail.com>
2023-02-01 13:14:24 -0800

    ItemRow View added with thumbnail, name and price (from:MenuItem model )

---
+++ ItemRow.swift
@@ -0,0 +1,30 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Rafael Levy on 2/1/23.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+                
+            }
+        }
+        
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
