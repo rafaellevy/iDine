cc47ae7c970ae709d3a40ae8354fb5b4d01c3cdc
Rafael Levy <srlevy2@gmail.com>
2023-02-01 11:23:23 -0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Rafael Levy on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
