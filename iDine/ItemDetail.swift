ba44c2daad2a11f525c03a047afc229e815a7f22
Rafael Levy <srlevy2@gmail.com>
2023-02-02 18:10:44 -0800

    ZStack added to fit photo credit

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -11,16 +11,38 @@ struct ItemDetail: View {
     let item: MenuItem
     
     var body: some View {
-        VStack {
-            Image(item.mainImage)
-            Text(item.description)
-        }
-        .navigationTitle(item.name)
+        
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(item.mainImage)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Text("Photo: \(item.photoCredit)")
+                        .background(.black)
+                        .padding(4)
+                        .font(.caption)
+                        .foregroundColor(.white)
+                        .offset(x: -5, y: -5)
+                }
+                
+                Text(item.description)
+                    .padding()
+                
+                Spacer()
+                
+            }.navigationTitle(item.name)
+        
+        
+        
     }
 }
 
 struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ItemDetail(item: MenuItem.example)
+        NavigationStack {
+            ItemDetail(item: MenuItem.example)
+        }
+        
     }
 }
